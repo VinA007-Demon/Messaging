9de8a9eef40efafbfa3ac495bdef8bfb70f4d8f7
Demon <asamvinayav@gmail.com>
2023-07-27 23:24:31 -0500

    US2 & US3 As a client, I want to send messages to the server./ As a client, I want to receive and display incoming messages.

--- Client.java
+++ Client.java
@@ -1,21 +1,40 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
     private static final String SERVER_HOST = "localhost";
-    private static final int SERVER_PORT = 8080;
 
     public static void main(String[] args){
-        try{
-            Socket socket = new Socket(SERVER_HOST, SERVER_PORT); //connect to the server
+        if(args.length < 1){
+            System.out.println("Usage: java Client <port number>");
+            return;
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+
+        try (
+            // Establish a connection to the server
+            Socket socket = new Socket(SERVER_HOST, portNumber);
+            // Set up output stream to send messages to the server
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            // Set up input stream to receive messages from the server
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        ) {
             System.out.println("Connected to the Server");
 
-            //Close the connection when done
-            //socket.close();
-            //System.out.println("Connection closed.");
-        }catch(IOException e){
-            e.printStackTrace();
+            // Send a message to the server
+            out.println("Hello Server!");
 
+            // Receive and print a message from the server
+            String response = in.readLine();
+            System.out.println("Server response: " + response);
+
+        } catch(IOException e){
+            e.printStackTrace();
         }
+        System.out.println("Connection closed.");
     }   
 }
